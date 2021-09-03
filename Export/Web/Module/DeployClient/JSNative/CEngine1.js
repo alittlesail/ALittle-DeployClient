4948de24ed1cd4a20ce00e10ef49ea78ad6d99d4
xingfanou <757609414@qq.com>
2021-09-03 18:08:20 +0800

    代码优化

--- Export/Web/Module/DeployClient/JSNative/CEngine1.js
+++ Export/Web/Module/DeployClient/JSNative/CEngine1.js
@@ -7125,7 +7125,6 @@ ALittle.TextEdit = JavaScript.Class(ALittle.DisplayObject, {
 	},
 	HandleMoveOut : function(event) {
 		this._move_in = false;
-		this._show.SetDisabled(!this._focus_in);
 		ALittle.System_SetNormalCursor();
 	},
 	set cursor_red(value) {
@@ -7651,7 +7650,6 @@ ALittle.TextInput = JavaScript.Class(ALittle.DisplayObject, {
 	},
 	HandleMoveOut : function(event) {
 		this._move_in = false;
-		this._show.SetDisabled(!this._focus_in);
 		ALittle.System_SetNormalCursor();
 	},
 	set cursor_red(value) {
